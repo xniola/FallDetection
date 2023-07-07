e0f62c920c6114fa9347fd20f0f225663d5df88a
xniola <stefano.perniola0@gmail.com>
2023-07-07 15:14:17 +0200

    pulizia repo

--- count2.sh
+++
@@ -1,9 +0,0 @@
-# Verifico se c'è matching preciso fra le immagini di cam0 e cam1
-# Se c'è matching preciso posso usare le stesse labels di cam0
-
-for i in $(seq -w 1 30 $END); do
- cam0=$(ls -l UR-Fall-Dataset-cam0-rgb-adl/train_images | grep fall-$i-cam0* | wc -l)
- cam1=$(ls -l cam1/fall-$i-cam1-rgb | wc -l)
- echo $cam0,$cam1; 
-done
-
